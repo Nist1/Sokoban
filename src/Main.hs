cd47428eb1eb0da0955aab24518f2ed4a9d7dccb
Степанов Никита <nstepanov508@gmail.com>
2024-11-27 22:48:51 +0300

    Changed key processing (keys to move 'wasd')

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 import Prelude hiding (Left, Right)
-ipmort System.Console.Terminfo
-import System.IO (hFlush, stdin)
-import Control.Monad (when)
+--import System.Console.Terminfo.Base
+import System.IO (hSetEcho, stdin)
+
 
 import GameField
 import LevelLoader
@@ -10,54 +10,58 @@ import Move
 main :: IO ()
 main = do
     putStrLn "Choose level 1 - 15:"
-    level <- getLine
-    gameField <- loadLevel (chooseLevel level)
+    l <- getLine
+    let level = chooseLevel l
+    gameField <- loadLevel level
     let goals = findGoalPositions gameField
+    --setupTerm
     gameLoop level gameField goals
-    
+    --cleanUpTerm
 
 gameLoop :: FilePath -> GameField -> [ElementPosition] -> IO ()
 gameLoop level gameField goals = do
+    --putStrLn "\ESC[2J" (Должно работать на Linux (Либо искать способ очистки предыдущего вывода)| На Windows не работает)
     printGameField gameField
     if isLevelSolved gameField goals
-        then putStrLn "Level solved successfully! Congratulations!" 
+        then putStrLn "Level solved successfully! Congratulations!"
         else do
             key <- getKey
-            case key of 
-                "q"   -> return ()
-                "ESC" -> return ()
-                "r"   -> do
+            case key of
+                'q' -> return ()
+                'r' -> do
                     putStrLn "Restarting level..."
                     gameField <- loadLevel level
                     gameLoop level gameField goals
-                let direction = case key of
-                        "\ESC[A" -> Just Up
-                        "\ESC[B" -> Just Down
-                        "\ESC[C" -> Just Left
-                        "\ESC[D" -> Just Right
-                        _        -> Nothing
-                case direction of
-                    Just direction -> gameLoop (handleMovement direction gameField) goals
-                    Nothing -> do
-                        putStrLn "Invalid move. Use arrow keys."
-                        gameLoop gameField goals
+                _ -> do
+                    let direction = case key of
+                            'w' -> Just Up
+                            's' -> Just Down
+                            'a' -> Just Left
+                            'd' -> Just Right
+                            _   -> Nothing
+                    case direction of
+                        Just dir -> do
+                            gameLoop level (handleMovement dir gameField) goals
+                        Nothing  -> do
+                            putStrLn "Invalid move. Use 'w', 'a', 's', 'd' keys."
+                            gameLoop level gameField goals
 
-setupTerm :: IO ()
-setupTerm = do
-    setupTermFromEnv
-    setEcho False
-    setCbreak True
+getKey :: IO Char
+getKey = do
+    hSetEcho stdin False
+    c <- getChar
+    case c of
+        '\n' -> getKey  -- Игнорируем символ новой строки и повторяем чтение
+        _    -> return c
 
-cleanupTerm :: IO ()
-cleanupTerm = do
-    setEcho True
-    setCbreak False
-    resetTerminalState
+-- setupTerm :: IO ()
+-- setupTerm = do
+--     setupTermFromEnv
+--     setEcho False
+--     setCbreak True
 
-getKey :: IO String
-getKey = reverse <$> getKey' ""
-    where
-        getKey' chars = do
-            char <- getChar
-            more <- hReady stdin ( if more then getKey' else return) 
-            (char : chars)
+-- cleanupTerm :: IO ()
+-- cleanupTerm = do
+--     setEcho True
+--     setCbreak False
+--     resetTerminalState
